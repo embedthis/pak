741ecd0650a9b5cbdf87409a13687f82b5a845ca
Embedthis Software <dev@embedthis.com>
2013-12-27 14:43:49 -0800

    FEATURE: Add Auth.check confirmation after restarting browser session.

--- src/catalog/client/app/auth/Auth.js
+++ src/catalog/client/app/auth/Auth.js
@@ -5,6 +5,7 @@
 
 angular.module('app').factory('Auth', function (EspResource) {
     return EspResource.group("auth", {}, {
+        'check':  { 'method': 'POST', url: '/:prefix/:controller/check' },
         'login':  { 'method': 'POST', url: '/:prefix/:controller/login' },
         'logout': { 'method': 'POST', url: '/:prefix/:controller/logout' },
     });
