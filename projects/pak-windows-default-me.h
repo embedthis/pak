8c89a47b313ffe86665b52c4a827c08245f32772
Embedthis Software <dev@embedthis.com>
2014-11-21 14:03:54 -0800

    FIX: windows projects using openssl

--- projects/pak-windows-default-me.h
+++ projects/pak-windows-default-me.h
@@ -223,7 +223,7 @@
 
 /* Profile */
 #ifndef ME_CONFIG_CMD
-    #define ME_CONFIG_CMD "me -d -q -platform windows-x86-default -configure . --with openssl -gen vs"
+    #define ME_CONFIG_CMD "me -d -q -platform windows-x86-default -configure . --with est -gen vs"
 #endif
 #ifndef ME_PAK_PRODUCT
     #define ME_PAK_PRODUCT 1
@@ -257,7 +257,7 @@
     #define ME_COM_EJS 1
 #endif
 #ifndef ME_COM_EST
-    #define ME_COM_EST 0
+    #define ME_COM_EST 1
 #endif
 #ifndef ME_COM_HTTP
     #define ME_COM_HTTP 1
@@ -272,7 +272,7 @@
     #define ME_COM_MPR 1
 #endif
 #ifndef ME_COM_OPENSSL
-    #define ME_COM_OPENSSL 1
+    #define ME_COM_OPENSSL 0
 #endif
 #ifndef ME_COM_OSDEP
     #define ME_COM_OSDEP 1
