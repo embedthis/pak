b269c348021e87d71d56467c0551a09686978ee7
Embedthis Software <dev@embedthis.com>
2015-05-11 17:07:02 -0700

    UPGRADE: http

--- src/ejs/ejsLib.c
+++ src/ejs/ejsLib.c
@@ -36975,7 +36975,10 @@ static bool waitForState(EjsHttp *hp, int state, MprTicks timeout, int throw)
             if (httpNeedRetry(conn, &url)) {
                 if (url) {
                     httpRemoveHeader(conn, "Host");
-                    location = httpCreateUri(url, 0);
+                    if ((location = httpCreateUri(url, 0)) == 0) {
+                        ejsThrowIOError(ejs, "Bad location Uri");
+                        return 0;
+                    }
                     uri = httpJoinUri(conn->tx->parsedUri, 1, &location);
                     hp->uri = httpUriToString(uri, HTTP_COMPLETE_URI);
                 }
@@ -49987,7 +49990,7 @@ static EjsUri *castToUri(Ejs *ejs, EjsObj *arg)
         arg = (EjsObj) ejsToString(ejs, arg);
         up->uri = httpCreateUri(up, ejsToMulti(ejs, arg), 0);
     }
-    if (!up->uri) {
+    if (!up->uri || !up->uri->valid) {
         ejsThrowArgError(ejs, "Invalid URI");
     }
     return up;
@@ -50008,6 +50011,9 @@ static EjsUri *cloneUri(Ejs *ejs, EjsUri *src, bool deep)
 #else
     dest->uri = httpCloneUri(src->uri, 0);
 #endif
+    if (!dest->uri || !dest->uri->valid) {
+        ejsThrowArgError(ejs, "Invalid URI");
+    }
     return dest;
 }
 
@@ -50883,7 +50889,7 @@ static EjsUri *uri_trimExt(Ejs *ejs, EjsUri *up, int argc, EjsObj **argv)
 static EjsObj *uri_set_uri(Ejs *ejs, EjsUri *up, int argc, EjsObj **argv)
 {
     up->uri = httpCreateUri(ejsToMulti(ejs, argv[0]), 0);
-    if (!up->uri) {
+    if (!up->uri || !up->uri->valid) {
         ejsThrowArgError(ejs, "Invalid URI");
     }
     return 0;
@@ -50953,7 +50959,7 @@ static HttpUri *toHttpUri(Ejs *ejs, EjsObj *arg, int dup)
         arg = (EjsObj*) ejsToString(ejs, arg);
         uri = httpCreateUri(ejsToMulti(ejs, arg), 0);
     }
-    if (!uri) {
+    if (!uri || !uri->valid) {
         ejsThrowArgError(ejs, "Invalid URI");
     }
     return uri;
@@ -51153,7 +51159,9 @@ PUBLIC void ejsConfigureUriType(Ejs *ejs)
         ejsBindMethod(ejs, type, ES_Uri_encode, uri_encode);
         ejsBindMethod(ejs, type, ES_Uri_encodeComponent, uri_encodeComponent);
         ejsBindMethod(ejs, type, ES_Uri_template, uri_template);
+#if ES_Uri_templateString
         ejsBindMethod(ejs, type, ES_Uri_templateString, uri_templateString);
+#endif
 
         prototype = type->prototype;
         ejsBindConstructor(ejs, type, uri_constructor);
