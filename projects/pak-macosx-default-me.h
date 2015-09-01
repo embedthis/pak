04cc139ef22afaf509f450407352b3402c6fd080
Embedthis Software <dev@embedthis.com>
2015-09-01 12:47:52 -0700

    UPGRADE: paks

--- projects/pak-macosx-default-me.h
+++ projects/pak-macosx-default-me.h
@@ -84,6 +84,9 @@
 #ifndef ME_EJS_ONE_MODULE
     #define ME_EJS_ONE_MODULE 1
 #endif
+#ifndef ME_EJSCRIPT___COMPILE
+    #define ME_EJSCRIPT___COMPILE "--debug"
+#endif
 #ifndef ME_EJSCRIPT_DB
     #define ME_EJSCRIPT_DB 0
 #endif
