6744c38deeb3dbc5a1de102839d1c5f4150b3efe
Michael O'Brien <dev@embedthis.com>
2019-01-11 18:20:55 +1100

    DEV: Gitlab building

--- paks/assist/index.js
+++ paks/assist/index.js
@@ -21,11 +21,11 @@ global.dump = (...args) => { for (let item of args) print(JSON.stringify(item, n
 var config = {}
 let argv = process.argv
 let files = [
+    '../CONFIG/keys.json',
+    'CONFIG/keys.json',
     '../pak.json',
     'pak.json',
     'product.json',
-    '../CONFIG/keys.json',
-    'CONFIG/keys.json',
 ]
 for (let file of files) {
     file = process.cwd() + '/' + file
@@ -49,17 +49,16 @@ for (let i = 2; i < argv.length; i++) {
     Set sensible defaults: Default to profile == dev, debug == true if dev.
  */
 config.profile = config.profile || process.env.PROFILE || 'dev'
-if (!config.profiles[config.profile]) {
-    util.log(`Cannot find profile "${config.profile}" in product.json`)
-    process.exit(1)
-}
 if (config.debug == 'true') {
     config.debug = true
 }
 if (config.debug !== true) {
     config.debug = (config.profile == 'dev') ? true : false
 }
-blend(config, config.profiles[config.profile])
+util.log(`Using profile "${config.profile}"`)
+if (config.profiles && config.profile && config.profiles[config.profile]) {
+    blend(config, config.profiles[config.profile])
+}
 
 /*
     Patch version from ../pak.json
@@ -75,6 +74,56 @@ try {
         print('Cannot read ../pak.json or ./pak.json')
     }
 }
-// util.log(`Select profile "${config.profile}", debug ${config.debug} ${config.version}`)
+
+global.expand = function(v, ...contexts) {
+    let json = JSON.stringify(v)
+    for (let context of contexts) {
+        json = template(json, context)
+    }
+    return JSON.parse(json)
+}
+
+function template(v, context) {
+    let text = v.toString()
+    let matches = text.match(/\$\{[^}]*}/gm)
+    if (matches) {
+        // context = clone(context)
+        for (let name of matches) {
+            let word = name.slice(2).slice(0, -1)
+            if (context[word] == undefined) {
+                context[word] = '${' + word + '}'
+            }
+        }
+        //  MOB - better to replace with code that does not eval
+        let fn = Function('_context_', 'with (_context_) { return `' + text + '`}')
+        return fn(context)
+    }
+    return text
+}
+
+function mapEnv(obj, prefix = '', vars = {}) {
+    for (let name of Object.keys(obj)) {
+        let value = obj[name]
+        if (name == 'profiles') {
+            continue
+        }
+        if (typeof value == 'object') {
+            mapEnv(value, prefix + name.toUpperCase() + '_', vars)
+        } else {
+            name = (prefix + name).toUpperCase().replace(/\./g, '_').replace(/-/g, '_')
+            vars[name] = value
+        }
+    }
+    return vars
+}
+
+global.getEnv = function(obj, context) {
+    let vars = mapEnv(obj)
+    for (let [index, v] of Object.entries(vars)) {
+        vars[index] = expand(v, context)
+    }
+    let env = blend({}, process.env)
+    return blend(env, vars)
+}
 
 export default config
