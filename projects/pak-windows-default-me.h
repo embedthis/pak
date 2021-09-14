8a38d0d209b9f5e38e4a5a47a2b55765feb5ccaa
Michael O'Brien <dev@embedthis.com>
2021-09-14 12:01:54 +1000

    DEV: regen projects

--- projects/pak-windows-default-me.h
+++ projects/pak-windows-default-me.h
@@ -133,7 +133,7 @@
     #define ME_TUNE "speed"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "1.0.5"
+    #define ME_VERSION "1.0.6"
 #endif
 
 /* Prefixes */
@@ -231,10 +231,10 @@
     #define ME_MINOR_VERSION 0
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 5
+    #define ME_PATCH_VERSION 6
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 100000005
+    #define ME_VNUM 100000006
 #endif
 
 /* Components */
